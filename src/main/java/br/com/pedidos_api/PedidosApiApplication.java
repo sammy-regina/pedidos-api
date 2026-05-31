62a999e9e8511b187241ffa419d438ed7df588ff
sammy-regina <145820828+sammy-regina@users.noreply.github.com>
2026-05-31 18:03:52 -0300

    feat: historia 1 criado projeto spring boot

--- src/main/java/br/com/pedidos_api/PedidosApiApplication.java
+++ src/main/java/br/com/pedidos_api/PedidosApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PedidosApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PedidosApiApplication.class, args);
 	}
 
