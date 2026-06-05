331719d3329370fffc852bac49512b031122cfb4
sammy-regina <145820828+sammy-regina@users.noreply.github.com>
2026-06-04 22:37:49 -0300

    feat: Criada a entidade cliente

---
+++ src/main/java/br/com/pedidos_api/entity/ClienteEntity.java
@@ -0,0 +1,37 @@
+package br.com.pedidos_api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_cliente")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClienteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 50)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(length = 20)
+    private String telefone;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+}
